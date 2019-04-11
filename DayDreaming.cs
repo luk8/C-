49c6371c910c58b7f709a4fc6a872f8f97cb2d52
luk8 <l.zetrenne88@gmail.com>
2019-04-10 21:58:00 -0400

    Add files via upload

---
+++ DayDreaming.cs
@@ -0,0 +1,34 @@
+﻿/* The program calucates the amount of time the user spends day dreaming*/
+
+using System;
+using static System.Console;
+
+class DayDreaming
+{
+    static void Main()
+    {
+        const int MIN_IN_HOUR = 60; // Constant is the number of minutes in an hour
+
+        // Declared minutes, hours and minutes left
+
+        int minutes;
+        int hours;
+        int minsLeft;
+
+        // User enters the amount of time spent day dreaming in minutes
+
+        Write("Enter the amount of minutes you spend day dreaming");
+
+        // Users entry is converted to an integer
+
+        minutes = Convert.ToInt32(ReadLine());
+
+        hours = minutes / MIN_IN_HOUR;
+
+        minsLeft = minutes % MIN_IN_HOUR;
+
+        // Displays the amount of time spent day dreaming in hours and minutes
+
+        WriteLine("You spend {0} hours and {1} minutes day dreaming", hours, minsLeft);
+    }
+}
